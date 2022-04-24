b3962a23daf8851bbc670b72244a9e77f818e5cc
MayceAlazzawi <maynab4@gmail.com>
2022-04-24 16:15:44 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(acti='roller-skate') {
+ return `This Saturday, I want to ${acti}!`
+}
+saturdayFun(acti='roller-skate')
+
+// code your solution here
+function mondayWork(acti='go to the office') {
+  return `This Monday, I will ${acti}.`
+ }
+ console.log(mondayWork('bathe my dog'))
+ 
+ let result = wrapAdjective('*')
+ let emphatic = result("a hard worker")
+ 
+ function wrapAdjective(ss='special') {
+  let result = wrapAdjective('*')
+      let emphatic = result("a hard worker")
+ }
+ console.log(mondayWork('bathe my dog'))
+ 
